2285e01e14ee515d17acac52fd63ca0dfab7b733
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-02-01 15:25:26 -0600

    routes and model update

--- models/Product.js
+++ models/Product.js
@@ -9,23 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: { 
+     id: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false, 
+        primaryKey: true, 
+        autoIncrement: true,
+  
+      },
 
-    },
+    
     
     product_name: { 
-
+      type: DataTypes.STRING, 
+      allowNull: false,
     }, 
 
     price: { 
-
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
+      validate: { 
+        isFloat: true,
+      }
     },
 
     stock: { 
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     }, 
     category_id: { 
-
+      type: DataTypes.INTEGER,
+      referneces: {
+        model: "category",
+        key: "id",
+      },
     }
     // define columns
   },
--- models/Tag.js
+++ models/Tag.js
@@ -6,9 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns 
+    // define columns  
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }, 
     tag_name: { 
-      
+      type: DataTypes.String,
+
     }
   },
   {
