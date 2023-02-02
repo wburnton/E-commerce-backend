b1b38b51196ce5939cf2aefb72d480ec78e225d1
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-02-01 18:23:33 -0600

    typo fixes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,12 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-    product_id: { 
+    // define columns 
+    id: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true,
+
+    },
+    
+    product_id: {  
+      type: DataTypes.INTEGER, 
+      references: { 
+        model: 'product', 
+        key: "id", 
+        unique: false
+      }
 
     }, 
     tag_id: { 
-      
+      type: DataTypes.INTEGER, 
+      references: { 
+        model: 'tag', 
+        key: "id", 
+        unique: false
+      }
     }
   },
   {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,12 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products 
   try { 
     const categoryData = await Category.findAll({
-      include: [
-        {
-        model: Product,
-        
-      }, 
-    ]
+      include: [{model: Product}]
   }); 
     res.status(200).json(categoryData);
   } catch (err) { 
@@ -25,12 +20,10 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products 
   try { 
-    const categoryData = await Category.findOne({
-      where: {
-        id: req.params.id,},
+    const categoryData = await Category.findByPk(req.params.id, {
         include: [{
           model: Product,
-        },],
+        }],
     }); 
     res.status(200).json(categoryData);
   } catch (err) { 
@@ -39,7 +32,7 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const newCategory = await Category.create({
@@ -51,7 +44,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value 
   try {
     const updatedCategory = await Category.update(req.body, {
@@ -69,7 +62,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value 
   try {
     const deletedCategory = await Category.destroy({
